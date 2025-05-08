07367fc49141cfc0c8768c18c556c95fd9147043
Mohamed Hussein <mohussein1279@gmail.com>
2025-05-08 13:47:29 +0300

    change the coroutine scope to runTest

--- src/test/kotlin/domain/useCases/GetClothesSuggestionsUseCaseTest.kt
+++ src/test/kotlin/domain/useCases/GetClothesSuggestionsUseCaseTest.kt
@@ -4,6 +4,7 @@ import io.mockk.coEvery
 import io.mockk.mockk
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runTest
 import org.example.domain.entities.*
 import org.example.domain.repositories.WeatherRepository
 import org.example.domain.useCases.GetClothesSuggestionsUseCase
@@ -15,77 +16,56 @@ class GetClothesSuggestionsUseCaseTest {
     private val useCase = GetClothesSuggestionsUseCase(weatherRepository)
 
     @Test
-    fun `execute() return  clothes suggestions based on weather`() {
-        runBlocking {
+    fun `execute() return  clothes suggestions based on weather`() = runTest {
+        //given
+        val location = Location(30.0444, 31.2357, "Cairo", "Egypt")
 
-            //given
-            val location = Location(30.0444, 31.2357, "Cairo", "Egypt")
+        val weather = WeatherData(
+            temperature = 38.0,
+            windSpeed = 10.0,
+            rain = 0.0,
+            humidity = 20.0,
+            isDay = true
+        )
+        val expected = listOf(
+            ClothItem("T-Shirt", ClothType.TOP, ClothWeight.LIGHT),
+            ClothItem("Shorts", ClothType.BOTTOM, ClothWeight.LIGHT),
+            ClothItem("Sunglasses", ClothType.TOP, ClothWeight.VERY_LIGHT),
+            ClothItem("Cap or Hat", ClothType.TOP, ClothWeight.LIGHT)
+        )
+        coEvery { weatherRepository.getWeatherDataByLocation(location) } returns weather
 
-            val weather = WeatherData(
-                temperature = 38.0,
-                windSpeed = 10.0,
-                rain = 0.0,
-                humidity = 20.0,
-                isDay = true
-            )
-            val expected = listOf(
-                ClothItem("T-Shirt", ClothType.TOP, ClothWeight.LIGHT),
-                ClothItem("Shorts", ClothType.BOTTOM, ClothWeight.LIGHT),
-                ClothItem("Sunglasses", ClothType.TOP, ClothWeight.VERY_LIGHT),
-                ClothItem("Cap or Hat", ClothType.TOP, ClothWeight.LIGHT)
-            )
-            coEvery { weatherRepository.getWeatherDataByLocation(location) } returns weather
-
-            // when
-            val result = useCase.execute(location)
-            //then
-            assertThat(result).isEqualTo(expected)
-
-
-        }
-
+        // when
+        val result = useCase.execute(location)
+        //then
+        assertThat(result).isEqualTo(expected)
     }
 
     @Test
-    fun `execute() throw exception when weather api failed`() {
-     //given
-        runBlocking {
-            val location = Location(30.0444, 31.2357, "Cairo", "Egypt")
-
-            coEvery { weatherRepository.getWeatherDataByLocation(location) } throws RuntimeException("Network error")
-          //when
-            val exception = assertThrows<RuntimeException> {
-                runBlocking {
-                    useCase.execute(location)
-                }
+    fun `execute() throw exception when weather api failed`() = runTest {
+        //given
+        val location = Location(30.0444, 31.2357, "Cairo", "Egypt")
+        coEvery { weatherRepository.getWeatherDataByLocation(location) } throws RuntimeException("Network error")
+        //when
+        val exception = assertThrows<RuntimeException> {
+            runBlocking {
+                useCase.execute(location)
             }
-
-
-         //then
-            assertThat("Network error").isEqualTo(exception.message)
-
         }
+        //then
+        assertThat("Network error").isEqualTo(exception.message)
     }
 
- @Test
- fun `execute() throw exception when location not found`() {
-    //given
-  val location = Location(0.0, 0.0, "Unknown", "Nowhere")
-
-  coEvery { weatherRepository.getWeatherDataByLocation(location) } throws IllegalArgumentException("Location not found")
-   //when
-  val exception = assertThrows<IllegalArgumentException> {
-   runBlocking {
-    useCase.execute(location)
-   }
-  }
-  //then
-
-  assertThat("Location not found").isEqualTo(exception.message)
- }
-
-
-
-
- }
-
+    @Test
+    fun `execute() throw exception when location not found`() = runTest {
+        //given
+        val location = Location(0.0, 0.0, "Unknown", "Nowhere")
+        coEvery { weatherRepository.getWeatherDataByLocation(location) } throws IllegalArgumentException("Location not found")
+        //when
+        val exception = assertThrows<IllegalArgumentException> {
+            useCase.execute(location)
+        }
+        //then
+        assertThat("Location not found").isEqualTo(exception.message)
+    }
+}
--- src/test/kotlin/domain/useCases/GetLocationByCityAndCountryTest.kt
+++ src/test/kotlin/domain/useCases/GetLocationByCityAndCountryTest.kt
@@ -2,94 +2,71 @@ package domain.useCases
 
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.runBlocking
 import org.example.domain.entities.Location
 import org.example.domain.repositories.LocationRepository
 import org.example.domain.useCases.GetLocationByCityAndCountry
 import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.assertThrows
 import kotlin.test.Test
 
-class GetLocationByCityAndCountryTest{
+class GetLocationByCityAndCountryTest {
 
-  private val locationRepository = mockk<LocationRepository>()
-  private val useCase = GetLocationByCityAndCountry(locationRepository)
+    private val locationRepository = mockk<LocationRepository>()
+    private val useCase = GetLocationByCityAndCountry(locationRepository)
 
-  @Test
-  fun `execute() return location for valid city and country`(){
-   runBlocking {
-    //given
+    @Test
+    fun `execute() return location for valid city and country`() = runTest {
+        //given
+        val city = "Cairo"
+        val country = "Egypt"
+        val expected = Location(30.0444, 31.2357, city, country)
 
-    val city = "Cairo"
-    val country = "Egypt"
-    val expected = Location(30.0444, 31.2357, city, country)
-
-    coEvery { locationRepository.getLocationByCityAndCountry(city, country) } returns expected
-
-   // when
-   val result = useCase.execute(city, country)
-    //then
-   assertThat(result).isEqualTo(expected)
-
-
-  }
-  }
-
-
- @Test
- fun `execute()  throw exception for not found country`(){
-   runBlocking {
-
-    //given
-    val city = "Atlantis"
-    val country = "UnknownLand"
-
-    coEvery { locationRepository.getLocationByCityAndCountry(city, country) } throws IllegalArgumentException("City not found")
-
-    //when
-
-    val exception= assertThrows<IllegalArgumentException> {
-       runBlocking { useCase.execute(city, country) }
+        coEvery { locationRepository.getLocationByCityAndCountry(city, country) } returns expected
+        // when
+        val result = useCase.execute(city, country)
+        //then
+        assertThat(result).isEqualTo(expected)
     }
 
-    //then
-     assertThat("country not found").isEqualTo(exception)
 
-   }
+    @Test
+    fun `execute()  throw exception for not found country`() = runTest {
+        //given
+        val city = "Atlantis"
+        val country = "UnknownLand"
+        coEvery {
+            locationRepository.getLocationByCityAndCountry(
+                city,
+                country
+            )
+        } throws IllegalArgumentException("City not found")
+        //when
+        val exception = assertThrows<IllegalArgumentException> {
+            useCase.execute(city, country)
+        }
 
-
-
-
- }
-
- @Test
- fun `execute()  throw exception when api failed`() {
-
-  //given
-  runBlocking {
-
-   val city = "Cairo"
-   val country = "Egypt"
-   coEvery { locationRepository.getLocationByCityAndCountry(city, country) } throws RuntimeException("Network failure")
-
-   //when
-   val exception = assertThrows<RuntimeException> {
-    runBlocking {
-     useCase.execute(city, country)
+        //then
+        assertThat("country not found").isEqualTo(exception)
     }
-  }
 
-   //then
-   assertThat("Network error").isEqualTo(exception)
+    @Test
+    fun `execute()  throw exception when api failed`() = runTest {
+        //given
+        val city = "Cairo"
+        val country = "Egypt"
+        coEvery {
+            locationRepository.getLocationByCityAndCountry(
+                city,
+                country
+            )
+        } throws RuntimeException("Network failure")
 
-
-
-  }
- }
-
+        //when
+        val exception = assertThrows<RuntimeException> {
+            useCase.execute(city, country)
+        }
+        //then
+        assertThat("Network error").isEqualTo(exception)
+    }
 }
-
-
-
-
-
