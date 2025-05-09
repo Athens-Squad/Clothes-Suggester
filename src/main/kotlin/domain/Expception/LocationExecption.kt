d11b87d45575798a7ed0e530859b6653ee7071bd
Sara Desouki <saradesouki39@gmail.com>
2025-05-09 21:40:14 +0300

    refactor exception

--- src/main/kotlin/domain/Expception/LocationExecption.kt
+++
@@ -1,6 +0,0 @@
-package org.example.domain.Expception
-
-class LocationExecption {
-    open class DomainExceptions(message: String) : Exception(message)
-    class NoLocationFoundException : DomainExceptions("No location found")
-}
