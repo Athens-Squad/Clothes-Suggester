083dcdf763384f77fb3badfd7feb9c1b32bc2b3b
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-05-10 12:45:28 +0300

    implement WeatherRepositoryImpl

--- src/main/kotlin/data/repositories/WeatherRepositoryImpl.kt
+++ src/main/kotlin/data/repositories/WeatherRepositoryImpl.kt
@@ -1,6 +1,7 @@
 package org.example.data.repositories
 
 import org.example.data.datasource.WeatherDataSource
+import org.example.data.mapper.mapToWeatherData
 import org.example.domain.entities.Location
 import org.example.domain.entities.WeatherData
 import org.example.domain.repositories.WeatherRepository
@@ -9,6 +10,7 @@ class WeatherRepositoryImpl(
     private val weatherDataSource: WeatherDataSource
 ): WeatherRepository {
     override suspend fun getWeatherDataByLocation(location: Location): WeatherData {
-        TODO("Not yet implemented")
+        val currentWeather = weatherDataSource.getWeatherData(location)
+        return currentWeather.mapToWeatherData()
     }
 }
