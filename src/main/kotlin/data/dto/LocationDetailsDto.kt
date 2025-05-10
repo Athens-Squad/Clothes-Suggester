e01359a078684fccb694db57b72ac1161003ce01
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-05-10 12:42:33 +0300

    refactor spaces

--- src/main/kotlin/data/dto/LocationDetailsDto.kt
+++ src/main/kotlin/data/dto/LocationDetailsDto.kt
@@ -5,16 +5,16 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class LocationDetailsDto(
-    @SerialName("id"           ) val id          : Int?    = null,
-    @SerialName("name"         ) val name        : String? = null,
-    @SerialName("latitude"     ) val latitude    : Double? = null,
-    @SerialName("longitude"    ) val longitude   : Double? = null,
-    @SerialName("elevation"    ) val elevation   : Int?    = null,
-    @SerialName("feature_code" ) val featureCode : String? = null,
-    @SerialName("country_code" ) val countryCode : String? = null,
-    @SerialName("timezone"     ) val timezone    : String? = null,
-    @SerialName("population"   ) val population  : Int?    = null,
-    @SerialName("country_id"   ) val countryId   : Int?    = null,
-    @SerialName("country"      ) val country     : String? = null
+    @SerialName("id") val id: Int? = null,
+    @SerialName("name") val name: String? = null,
+    @SerialName("latitude") val latitude: Double? = null,
+    @SerialName("longitude") val longitude: Double? = null,
+    @SerialName("elevation") val elevation: Int? = null,
+    @SerialName("feature_code") val featureCode: String? = null,
+    @SerialName("country_code") val countryCode: String? = null,
+    @SerialName("timezone") val timezone: String? = null,
+    @SerialName("population") val population: Int? = null,
+    @SerialName("country_id") val countryId: Int? = null,
+    @SerialName("country") val country: String? = null
 
 )
