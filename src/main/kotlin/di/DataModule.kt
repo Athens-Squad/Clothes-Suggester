227f0f45cf1d7af6907c50d7eb5d02e7ec819868
Nabil Bouzine <nabilsocialnetwork@gmail.com>
2025-05-10 12:56:20 +0100

    provide the dependencies with koin

--- src/main/kotlin/di/DataModule.kt
+++ src/main/kotlin/di/DataModule.kt
@@ -4,6 +4,7 @@ import org.example.data.datasource.LocationDataSource
 import org.example.data.datasource.WeatherDataSource
 import org.example.data.datasource.remote.LocationDataSourceImpl
 import org.example.data.datasource.remote.WeatherDataSourceImpl
+import org.example.data.mapper.LocationMapper
 import org.example.data.repositories.LocationRepositoryImpl
 import org.example.data.repositories.WeatherRepositoryImpl
 import org.example.domain.repositories.LocationRepository
@@ -11,7 +12,11 @@ import org.example.domain.repositories.WeatherRepository
 import org.koin.dsl.module
 
 val dataModule = module {
-    single<LocationRepository> { LocationRepositoryImpl(get()) }
+	single { LocationMapper() }
+    single<LocationRepository> { LocationRepositoryImpl(
+	    locationDataSource = get(),
+	    locationMapper = get()
+    ) }
     single<WeatherRepository> { WeatherRepositoryImpl(get()) }
 
     single<LocationDataSource> {LocationDataSourceImpl()}
